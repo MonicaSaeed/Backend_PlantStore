9963b98a085d7eae9bfc8fc7ba152c59e173729e
SaraElsayed <esara7772@gmail.com>
2025-05-01 16:02:32 +0300

    adduserMiddleware

--- Backend/app.js
+++ Backend/app.js
@@ -13,6 +13,9 @@ app.use('/pay', payRoutes);
 // Connect to MongoDB
 connectDB();
 
+const userRoute = require("../Backend/Routes/UserRoutes");
+app.use("/api/users",userRoute);
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
